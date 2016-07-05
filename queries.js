2020bf9d36d4d824787af638169dab6a749e3d13
varun-tandon <vrntandon@gmail.com>
2016-07-05 09:42:53 -0700

    Improved UI

---
+++ queries.js
@@ -0,0 +1,342 @@
+var server = require("./server.js");
+var md5 = require('blueimp-md5').md5;
+var sha256 = require('sha256');
+var fs = require('fs'); //needed to read connection json file
+
+//msyql setup
+var mysql = require('mysql');
+
+//create mysql pool
+var pool = mysql.createPool(JSON.parse(fs.readFileSync("./static/private_json/mysql_connection.json", "utf8")));
+
+function getHomepageNumbers(callback) {
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            throw err;
+        }
+
+        var querystring = "SELECT COUNT(*) FROM Biomolecules;" +
+                          "SELECT COUNT(*) FROM Diseases;" +
+                          "SELECT COUNT(*) FROM Users;";
+        connection.query(querystring, function(err, rows, fields) {
+            if (err) {
+                throw err;
+                return null;
+            }
+
+            callback ({
+                NUM_BIOMARKERS_SERVER: rows[0][0]["COUNT(*)"],
+                NUM_DISEASES_SERVER: rows[1][0]["COUNT(*)"],
+                NUM_USERS_SERVER: rows[2][0]["COUNT(*)"]
+            });
+        });
+
+        connection.release();
+    });
+}
+
+
+function getDossierInfo(loggedIn, emailIfLoggedIn, biomarkerName, response, callback) {
+    var querystring = "";
+    var biomarkerInfo = {
+        Name: biomarkerName
+    };
+
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            throw err;
+        }
+
+        querystring = "SELECT fk_Biomolecules FROM Biomolecule_Names WHERE Name=?;"; //? used to prevent SQL injection
+        connection.query(querystring, [biomarkerName], function(err, rows, fields) {
+            if (err) {
+                throw err;
+            }
+
+            if( !(typeof rows != "undefined" && rows != null && rows.length > 0) ) {
+                response.render('pages/index', {
+                    search_failed: true,
+                    biomarkerName: biomarkerName,
+                    num_biomarkers: server.homepageStats.NUM_BIOMARKERS_SERVER,
+                    num_diseases: server.homepageStats.NUM_DISEASES_SERVER,
+                    num_users: server.homepageStats.NUM_USERS_SERVER,
+                    logged_in: loggedIn,
+                    email: emailIfLoggedIn
+                });
+            }
+            else {
+                biomarkerInfo["Biomolecule MySQL ID"] = rows[0].fk_Biomolecules;
+
+                querystring = "SELECT * from Biomolecules WHERE pk_Biomolecules=" + rows[0].fk_Biomolecules + ";" +
+                              "SELECT Name from Biomolecule_Names where fk_Biomolecules=" + rows[0].fk_Biomolecules + ";" +
+                              "SELECT * from Biomolecules_Sources_Association where fk_Biomolecules=" + rows[0].fk_Biomolecules + ";";
+                connection.query(querystring, function(err, rows, fields) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    //flat arrays
+                    biomarkerInfo["Alternate Names"] = [];
+
+                    //getting alternate names for biomarker
+                    for(var jsonE in rows[1]) {
+                        var newName = rows[1][jsonE].Name;
+                        if(biomarkerName.toUpperCase() !== newName.toUpperCase())
+                            biomarkerInfo["Alternate Names"].push(newName);
+                        else
+                            biomarkerInfo['Name'] = newName;
+                    }
+
+                    //array of objects
+                    biomarkerInfo["Sources"] = [{}];
+
+                    //could be multiple sources for one biomarker - begin reading info into sources section of biomarkerInfo
+                    for(var jsonE in rows[2]) {
+                        biomarkerInfo['Sources'][jsonE]["Source MySQL ID"] = rows[2][jsonE].fk_Sources;
+
+                        //load array of trial objects into individual paper object
+                        biomarkerInfo['Sources'][jsonE]["Trials"] = [];
+                        querystring = "SELECT * from Trial WHERE fk_Sources=" + rows[2][jsonE].fk_Sources + ";";
+                        connection.query(querystring, function(err, rows, fields) {
+                            if (err) {
+                                throw err;
+                            }
+
+                            for (var i = 0; i < rows.length; i++) {
+                                var numPatients = rows[i].Patients;
+                                querystring = "SELECT Discovery_Method from Discovery_Method WHERE pk_Discovery_Method=" + rows[i].fk_Discovery_Method + ";";
+                                connection.query(querystring, function(err, rows, fields) {
+                                    if (err) {
+                                        throw err;
+                                    }
+
+                                    biomarkerInfo['Sources'][jsonE]["Trials"].push({
+                                        '# Patients': numPatients,
+                                        'Discovery Method': rows[0].Discovery_Method
+                                    });
+                                });
+                            }
+
+                        });
+
+                        //create flat arr of other biomarkers
+                        biomarkerInfo['Sources'][jsonE]["Other Biomarkers"] = [];
+
+                        biomarkerInfo["Sources"][jsonE]["Biomolecule State"] = rows[2][jsonE].Biomolecule_State;
+                        biomarkerInfo["Sources"][jsonE]["Drug Name"] = rows[2][jsonE].Drug_Name;
+
+                        querystring = "SELECT Disease from Diseases WHERE pk_Diseases=" + rows[2][jsonE].fk_Diseases + ";" +
+                                      "SELECT Purpose FROM Biomarker_Purpose WHERE pk_Biomarker_Purpose=" + rows[2][jsonE].fk_Biomarker_Purpose + ";" +
+                                      "SELECT * FROM Sources WHERE pk_Sources=" + rows[2][jsonE].fk_Sources + ";" +
+                                      "SELECT fk_Biomolecules FROM Biomolecules_Sources_Association WHERE fk_Sources=" + rows[2][jsonE].fk_Sources + ";";
+                        connection.query(querystring, function(err, rows, fields) {
+                            if (err) {
+                                throw err;
+                            }
+
+                            biomarkerInfo['Sources'][jsonE]["Disease"] = rows[0][jsonE].Disease;
+                            biomarkerInfo['Sources'][jsonE]["Purpose"] = rows[1][jsonE].Purpose;
+                            biomarkerInfo['Sources'][jsonE]["Google ID"] = rows[2][jsonE].Google_ID;
+                            biomarkerInfo['Sources'][jsonE]["URL"] = rows[2][jsonE].URL;
+
+                            querystring = "SELECT Source_Type from Source_Type WHERE pk_Source_Type=" + rows[2][jsonE].fk_Source_Type + ";" +
+                                          "SELECT Original_Source_Database from Original_Source_Databases WHERE pk_Original_Source_Databases=" + rows[2][jsonE].fk_Original_Source_Databases + ";";
+                            //getting all other biomarkers associated w source
+                            for(var i = 0; i < rows[3].length; i++)
+                                querystring += "SELECT Name, fk_Biomolecules from Biomolecule_Names WHERE fk_Biomolecules=" + rows[3][i].fk_Biomolecules + " LIMIT 1 ;";
+                            connection.query(querystring, function(err, rows, fields) {
+                                if (err) {
+                                    throw err;
+                                }
+
+                                biomarkerInfo['Sources'][jsonE]["Source Type"] = rows[0][jsonE].Source_Type;
+                                biomarkerInfo['Sources'][jsonE]["Original Source Database"] = rows[1][jsonE].Original_Source_Database;
+
+                                //adding all other biomarkers associated w paper, excluding current one
+                                for(var i = 2; i < rows.length; i++) {
+                                    if(biomarkerInfo["Biomolecule MySQL ID"] != rows[i][0].fk_Biomolecules)
+                                        biomarkerInfo['Sources'][jsonE]["Other Biomarkers"].push(rows[i][0].Name);
+                                }
+
+                                //send info to server.js to display to view - put here because this is the longer query cascade
+                                callback(biomarkerInfo);
+                            });
+                        });
+                    }
+
+                    querystring = "SELECT Medium from Biomolecule_Medium WHERE pk_Biomolecule_Medium=" + rows[0][0].fk_Biomolecule_Medium + ";" +
+                                  "SELECT Type from Biomolecule_Type WHERE pk_Biomolecule_Type=" + rows[0][0].fk_Biomolecule_Type + ";";
+                    connection.query(querystring, function(err, rows, fields) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        biomarkerInfo['Medium'] = rows[0][0]['Medium'];
+                        biomarkerInfo['Type'] = rows[1][0]['Type'];
+                    });
+                });
+            }
+        });
+    });
+}
+
+function getSQLDate(callback) {
+    var currentDayTwoDigits = new Date().getMonth() + 1;
+    if (currentDayTwoDigits < 10)
+        currentDayTwoDigits = "0" + currentDayTwoDigits.toString();
+    currentDate = new Date().getFullYear().toString() + "-" + currentDayTwoDigits + "-" + new Date().getDate().toString();
+    callback(currentDate);
+}
+
+function createUnverifiedAccount(accountInfo, response, callback) {
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            throw err;
+        }
+
+        querystring = "SELECT * FROM Users WHERE email=?;";
+        connection.query(querystring, [accountInfo.email], function(err, rows, fields) {
+            if(typeof rows != "undefined" && rows != null && rows.length > 0) {
+                response.render('pages/account-creation/signup', {
+                    create_account_problem_text: "An account with the entered email already exists. Please try again.",
+                    logged_in: false
+                });
+            }
+            else {
+
+                querystring = "INSERT INTO Users (firstName, lastName, email, pass, date_signed_up, hash, verified) " +
+                              "VALUES (?, ?, ?, ?, ?, ?, false);";
+                getSQLDate(function(currentDate) {
+                    accountInfo.date = currentDate;
+                    accountInfo.hash = md5(accountInfo.lastName + accountInfo.email);
+                    connection.query(querystring, [accountInfo.firstName, accountInfo.lastName, accountInfo.email, sha256(accountInfo.email + accountInfo.pass), accountInfo.date, accountInfo.hash], function(err, rows, fields) {
+                        if (err) {
+                            throw err;
+                        }
+                        console.log("user with email " + accountInfo.email + " created unverified account.");
+                        callback(); //created to success, success view on server.js
+                    });
+                });
+            }   
+        });
+    });
+}
+
+function verifyAccount(accountInfo, response, callback) {
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            throw err;
+        }
+
+        var validFields = true;
+
+        if (accountInfo.email == undefined) {
+            validFields = false;
+            response.render("pages/account-creation/verify-account", {
+                create_account_problem_text: "Whoops, something went wrong. Sorry! Please contact us with details about your account creation.",
+                logged_in: false
+            });
+            callback();
+        }
+        if (accountInfo.hash == undefined) {
+            validFields = false;
+            response.render("pages/account-creation/verify-account", {
+                create_account_problem_text: "Whoops, something went wrong. Sorry! Please contact us with details about your account creation.",
+                logged_in: false
+            });
+            callback();
+        }
+
+        if(validFields)
+        {
+            querystring = "SELECT * FROM Users WHERE email=? AND hash=?;";
+            connection.query(querystring, [accountInfo.email, accountInfo.hash], function(err, rows, fields) {
+                if (err) {
+                    throw err;
+                }
+
+                if(!(typeof rows != "undefined" && rows != null && rows.length > 0)) {
+                    response.render("pages/account-creation/verify-account", {
+                        create_account_problem_text: "Whoops, something went wrong. Sorry! Please contact us with details about your account creation.",
+                        logged_in: false
+                    });
+                }
+                else {
+                    accountInfo.name = rows[0].firstName;
+
+                    querystring = "UPDATE Users SET verified=true WHERE email=? AND hash=?;";
+                    connection.query(querystring, [accountInfo.email, accountInfo.hash], function(err, rows, fields) {
+                        if (err) {
+                            throw err;
+                        }
+                        response.render("pages/account-creation/verify-account", {
+                            name: accountInfo.name,
+                            email: accountInfo.email,
+                            logged_in: false
+                        }); //success
+                        console.log("account with email " + accountInfo.email + " verified");
+                    });
+                }
+                callback();
+            });
+        }
+    });
+}
+
+function loginUser(accountInfo, response, callback) {
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            throw err;
+        }
+        querystring = "SELECT * FROM Users WHERE email=? AND pass=?;";
+        connection.query(querystring, [accountInfo.email, sha256(accountInfo.email + accountInfo.pass)], function(err, rows, fields) {
+            if (err) {
+                throw err;
+            }
+
+            if(!(typeof rows != "undefined" && rows != null && rows.length > 0)) {
+                response.render('pages/login', {
+                    login_problem_text: "Invalid login. Please try again.",
+                    logged_in: false
+                });
+            }
+            else if(rows[0].verified == 0) {
+                response.render('pages/account-creation/unverified', {
+                    email: accountInfo.email,
+                    sendingVerificationEmail: false,
+                    logged_in: false
+                }); 
+            }
+            else {
+                callback();
+            }
+        });
+    });
+}
+
+function getAccountInfo(email, callback) {
+    var accountInfo = {};
+    accountInfo.email = email;
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            throw err;
+        }
+        querystring = "SELECT * FROM Users WHERE email=?;";
+        connection.query(querystring, [accountInfo.email], function(err, rows, fields) {
+            accountInfo.firstName = rows[0].firstName;
+            accountInfo.lastName = rows[0].lastName;
+            callback(accountInfo);
+        });
+    });
+}
+
+function addNewBiomarker(biomarkerInfo, callback) {
+    
+}
+
+module.exports.loginUser = loginUser;
+module.exports.getHomepageNumbers = getHomepageNumbers;
+module.exports.getDossierInfo = getDossierInfo;
+module.exports.createUnverifiedAccount = createUnverifiedAccount;
+module.exports.verifyAccount = verifyAccount;
+module.exports.getAccountInfo = getAccountInfo;
---
+++ server.js
@@ -0,0 +1,250 @@
+var queries = require("./queries.js");
+var mailer = require("./mailer.js");
+
+var querystring = "";
+var port = 7777;
+
+//express setup
+var express = require('express');
+var app = express();
+
+//add body parser to read post requests, cookie parser to read cookies
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());         // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+//set view engine to ejs
+app.set('view engine', 'ejs');
+
+//give static access to 'static' directory
+app.use(express.static('static'));
+
+queries.getHomepageNumbers(function(homepageStats) {
+    module.exports.homepageStats = homepageStats;
+
+    //routing index
+    app.get('/', function (request, response) {
+        console.log('got a GET request from index');
+        var loggedIn = (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined);
+
+        var biomarkerName = request.query['biomarker'];
+        if (biomarkerName == undefined) {
+            response.render('pages/index', {
+                search_failed: false,
+                num_biomarkers: homepageStats.NUM_BIOMARKERS_SERVER,
+                num_diseases: homepageStats.NUM_DISEASES_SERVER,
+                num_users: homepageStats.NUM_USERS_SERVER,
+                logged_in: loggedIn,
+                email: request.cookies.email
+            });
+        }
+        else {
+            queries.getDossierInfo(loggedIn, request.cookies.email, biomarkerName, response, function(biomarkerInfo) {
+                console.log("search returned:");
+                console.log(biomarkerInfo);
+                response.render('pages/dossier', {
+                    biomarkerInfo: biomarkerInfo,
+                    logged_in: loggedIn,
+                    email: request.cookies.email
+                });
+            });
+        }
+    });
+    
+    //routing editor mode
+    app.get('/contribute', function (request, response) {
+        console.log('got a GET request from edit');
+        var loggedIn = (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined);
+        if (!loggedIn) {
+            response.render('pages/already_logout', {
+                logged_in: false
+            });
+        } else {
+            response.render('pages/edit-main', {
+                logged_in: true,
+                email: request.cookies.email
+            });
+        }
+    });
+    app.post('contribute', function (request, response) {
+        console.log('got a contribute POST request');
+        var loggedIn = (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined);
+        if (!loggedIn) {
+            response.render('pages/already_logout', {
+                logged_in: false
+            });
+        } else {
+            console.log(request.body);
+        }
+    });
+
+    //routing about
+    app.get("/about", function (request, response) {
+        console.log('got a GET request from about');
+        response.render('pages/about', {
+            logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+            email: request.cookies.email
+        });
+    });
+
+    //routing account-info
+    app.get("/account-info", function (request, response) {
+        console.log('got a GET request from account-info');
+        var loggedIn = (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined);
+        if (loggedIn) {
+            queries.getAccountInfo(request.cookies.email, function(accountInfo) {
+                response.render('pages/account-info', {
+                    accountInfo: accountInfo,
+                    logged_in: true,
+                    email: request.cookies.email
+                });
+            })
+        } else {
+            response.render('pages/already_logout', {
+                logged_in: false
+            });
+        }
+    });
+
+    //routing login
+    app.get("/login", function (request, response) {
+        console.log('got a GET request from login');
+        var loggedIn = (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined);
+        if(!loggedIn) {
+            response.render('pages/login', {
+                logged_in: false
+            });
+        } else {
+            response.render('pages/already_login', {
+                logged_in: true,
+                email: request.cookies.email
+            });
+        }
+    });
+    app.post('/login', function (request, response) {
+        console.log("got a login POST request");
+
+        queries.loginUser({
+            email: request.body.user.email,
+            pass: request.body.user.pwd
+        }, response, function() {
+            //do something, set a cookie, whatever
+            response.cookie('email', request.body.user.email);
+            response.redirect("../"); //redirect to index
+        });
+    });
+
+    app.get('/logout', function (request, response) {
+        console.log("got a GET request from logout");
+        var email_address = request.cookies.email;
+        if(typeof request.cookies.email !='undefined' && request.cookies.email !=undefined) {
+            response.clearCookie("email");
+            response.redirect("/");
+        }
+    });
+
+    //routing signup
+    app.get('/signup', function (request, response) {
+        response.render('pages/account-creation/signup', {
+            logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+            email: request.cookies.email
+        });
+        console.log('got a GET request from account-creation/signup');
+    });
+    app.post('/signup', function (request, response) {
+        console.log("got a account-creation/signup POST request");
+        if (request.body.user.pwd !== request.body.user.repeatpwd) {
+            response.render('pages/account-creation/signup', {
+                create_account_problem_text: "Passwords did not match. Please try again.",
+                logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+                email: request.cookies.email
+            });
+        }
+        else if (request.body.user.firstname.length == 0) {
+            response.render('pages/account-creation/signup', {
+                create_account_problem_text: "First Name is a required field and is left blank.",
+                logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+                email: request.cookies.email
+            });
+        }
+        else if (request.body.user.lastname.length == 0) {
+            response.render('pages/account-creation/signup', {
+                create_account_problem_text: "Last Name is a required field and is left blank.",
+                logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+                email: request.cookies.email
+            });
+        }
+        else if (request.body.user.pwd.length < 5) {
+            response.render('pages/account-creation/signup', {
+                create_account_problem_text: "Password must contain at least 5 characters. Please try again.",
+                logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+                email: request.cookies.email
+            });
+        }
+        else {
+            accountInfo = {
+                firstName: request.body.user.firstname,
+                lastName: request.body.user.lastname,
+                email: request.body.user.email,
+                pass: request.body.user.pwd
+            }
+
+            queries.createUnverifiedAccount(accountInfo, response, function() {
+                response.render('pages/account-creation/unverified', {
+                    email: request.body.user.email,
+                    sendingVerificationEmail: true,
+                    logged_in: false
+                });
+
+                mailer.sendVerificationEmail(accountInfo, function() {
+                    //do something after email is sent
+                });
+            });
+        }
+    });
+
+    //routing account-creation/verify-account (users come here from email message)
+    app.get("/account-creation/verify-account", function (request, response) {
+        console.log('got a GET request from account-creation/verify-account');
+
+        queries.verifyAccount({
+            email: request.query.email,
+            hash: request.query.hash
+        }, response, function() { 
+            //do something??
+        });
+    });
+
+    // routing 404 event
+    app.use(function (request, response) {
+        response.status(400);
+        response.render('pages/404', {
+            logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+            email: request.cookies.email
+        });
+    });
+      
+    // routing 500 event
+    app.use(function (error, request, response, next) {
+        response.status(500);
+        response.render('pages/500', {
+            logged_in: (typeof request.cookies.email !='undefined' && request.cookies.email !=undefined),
+            email: request.cookies.email
+        });
+        console.log("Internal server error: \n" + error);
+    });
+
+}); //from getHomepageNumbers
+
+//listen on localhost
+app.listen(port);
+console.log('listening on http://localhost:' + port);
+
+//end mysql pool
+process.on('exit', function(code) {
+    pool.end();
+});
